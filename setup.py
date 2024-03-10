6a98af10a954b4d4779d7bf711bde9c2129d2df0
Gallen Wang <gallen.yes@gmail.com>
2024-03-10 19:24:25 +0800

    fix setup bug

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import json
 print('Thanks for using DiscordAutoSender!')
 print('Repository: https://github.com/gallen881/DiscordAutoSender')
 
-config = []
+config = {}
 
 config['bot_token'] = input('Enter your bot token:?')
 config['user_token'] = input('Enter your user token:?')
